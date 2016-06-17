3b4afd07a9ef1c157c4dea345e173f589dcda482
TakesxiSximada <takesxi.sximada@gmail.com>
2016-06-17 23:44:38 +0900

    add bootstrap file

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+mkdir -p unmake
+curl -L -o unmake/Makefile https://raw.githubusercontent.com/TakesxiSximada/unmake/develop/unmake/Makefile
+echo "Insert next line in your Makefile"
+echo
+echo "include unmake/Makefile"
+echo
