5af330c64120041b6da194258db876c1f0072e19
TakesxiSximada <takesxi.sximada@gmail.com>
2016-06-21 12:54:02 +0900

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 mkdir -p unmake
 curl -L -o unmake/Makefile https://raw.githubusercontent.com/TakesxiSximada/unmake/develop/unmake/Makefile
-echo "Insert next line in your Makefile"
-echo
+echo "Insert next lines in your Makefile top"
+echo "#-------------------------------------"
+echo "UNMAKE_CURRENT_MAKEFILE := Makefile"
 echo "include unmake/Makefile"
-echo
+echo "#-------------------------------------"
